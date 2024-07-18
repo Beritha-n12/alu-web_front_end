e266a56c824a8f00153adb2055c804de754a29a6
Beritha Niyotwagira <b.niyotwagi@alustudent.com>
2024-07-18 14:19:37 +0300

    changes

--- Javascript_advanced/2-function_me.js
+++
@@ -1,7 +0,0 @@
-function welcomeMessage(fullName) {
-  return () => alert(`Welcome ${fullName}!`);
-}
-
-const guillaume = welcomeMessage("Guillaume");
-const alex = welcomeMessage("Alex");
-const fred = welcomeMessage("Fred");
